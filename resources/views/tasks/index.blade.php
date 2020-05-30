554c896694b700aaad55a77baf76be13d43b9079
Ubuntu <ubuntu@ip-172-31-32-9.ap-northeast-1.compute.internal>
2020-05-30 19:17:20 +0000

    add status

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                    <th>進捗状況</th>
+                    <th>登録日</th>
+                    <th>更新日</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    {{-- タスク詳細ページへのリンク --}}
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
+                    <td>{{ $task->created_at }}</td>
+                    <td>{{ $task->updated_at }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+    {{-- ページネーションのリンク --}}
+    {{ $tasks->links() }}
+
+    {{-- タスク作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
