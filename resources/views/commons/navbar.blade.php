31327147c21d17d0a5b572875919100fa2cd0b9d
Ubuntu <ubuntu@ip-172-31-32-9.ap-northeast-1.compute.internal>
2020-05-31 13:13:15 +0000

    top page auth

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,8 +10,22 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}さんがログイン中</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- タスク作成ページへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
+@if (Auth::check())
     <h1>タスク一覧</h1>
-
     @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
@@ -35,5 +35,17 @@
 
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+
+@else
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>TaskList</h1>
+            <p>これはタスク管理用のアプリです。</p>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'SignUp', [], ['class' => 'btn btn-lg btn-primary mr-5']) !!}
+            {!! link_to_route('login', 'Login', [], ['class' => 'btn btn-lg btn-success']) !!}
+        </div>
+    </div>
+@endif
 
 @endsection
